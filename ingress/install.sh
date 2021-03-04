206bb899532eeaf07956c155087d5a3c91ade204
Marco Ippel <marco@ippelsoft.nl>
2021-03-04 21:26:58 +0100

    update

--- ingress/install.sh
+++ ingress/install.sh
@@ -1,36 +1,35 @@
-# Create the namespace for the ingress controller
-kubectl create namespace ingress
+# Create a namespace for your ingress resources
+kubectl create namespace ingress-basic
 
-# Add the repository to helm
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+# Add the ingress-nginx repository
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
 # Install the nginx ingress controller
-helm install stable/nginx-ingress \
-    --namespace wordpress \
+helm install ingress ingress-nginx/ingress-nginx \
+    --namespace ingress-basic \
     --set controller.replicaCount=1 \
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
-    --set controller.service.loadBalancerIP="104.45.69.115" \
-    ingress-demo
+    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux
 
 # Install the CustomResourceDefinition resources separately
-kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
+#kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
 
 # Create the namespace for cert-manager
-kubectl create namespace cert-manager
+#kubectl create namespace cert-manager
 
 # Label the cert-manager namespace to disable resource validation
-kubectl label namespace cert-manager cert-manager.io/disable-validation=true
+#kubectl label namespace cert-manager cert-manager.io/disable-validation=true
 
 # Add the Jetstack Helm repository
-helm repo add jetstack https://charts.jetstack.io
+#helm repo add jetstack https://charts.jetstack.io
 
 # Update your local Helm chart repository cache
-helm repo update
+#helm repo update
 
 # Install the cert-manager Helm chart
-helm install \
-  --namespace cert-manager \
-  --version v0.11.0 \
-  jetstack/cert-manager \
-  --generate-name
+# helm install \
+#   --namespace cert-manager \
+#   --version v0.11.0 \
+#   jetstack/cert-manager \
+#   --generate-name
--- scripts/install_wp_helm.sh
+++
@@ -1,5 +0,0 @@
-#bin/bash
-helm install wp-demo2 ./../helm/demo/ \
---namespace wordpress \
---set host=your-host \
---set mysqlPass=password
